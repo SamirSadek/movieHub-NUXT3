c4d77aef124daff8274895a6e5590df3a13af5bb
Samir Sadek <samirsadek2009@gmail.com>
2023-12-28 20:58:44 +0600

    project setup with nuxt and tailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+           <h2 class="text-red-600 font-bold">This is Homepage</h2>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
